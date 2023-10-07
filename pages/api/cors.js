c5e2959b39cdc47b5c78fa9930fafdc0e48eb08f
Egor Malishevski <44815359+egor-malishevski@users.noreply.github.com>
2023-10-08 02:48:16 +0300

    Update cors.js

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -1,5 +1,11 @@
 import fetch from "isomorphic-unfetch";
 
+export const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
 const Cors = async (req, res) => {
   const { url } = req.query;
   
@@ -9,13 +15,13 @@ const Cors = async (req, res) => {
 
        const data = await resProxy.json();
     
-       res.status(200).json({ data });
+       res.status(200).json({ data }, { headers: corsHeaders });
     } else {
        const resProxy = await fetch(url, { ...req, "mode": "no-cors" });
 
        const data = await resProxy.text();
 
-       res.status(200).json({ data });
+       res.status(200).json({ data }, { headers: corsHeaders });
     }
   } catch (error) {
     res.status(400).send(error.toString());
