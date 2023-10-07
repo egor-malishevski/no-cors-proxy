b52e075d7fd0da8b0b08da51f7a3ee4fe14883e6
Egor Malishevski <44815359+egor-malishevski@users.noreply.github.com>
2023-10-08 02:30:50 +0300

    Update cors.js

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -5,13 +5,13 @@ const Cors = async (req, res) => {
   
   try {
     if (req.method === 'GET' || req.method === 'HEAD') {
-       const resProxy = await fetch(url, { headers: req.headers, method: req.method, "mode": "no-cors" });
+       const resProxy = await fetch(url, { "mode": "no-cors" });
 
        const data = await resProxy.json();
     
        res.status(200).json({ data });
     } else {
-       resProxy = await fetch(url, { ...req, "mode": "no-cors" });
+       const resProxy = await fetch(url, { ...req, "mode": "no-cors" });
 
        const data = await resProxy.json();
 
