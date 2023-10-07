33f495ec670a315eae9668aeb6ba4166333f1b02
Egor Malishevski <44815359+egor-malishevski@users.noreply.github.com>
2023-10-08 01:34:34 +0300

    Update cors.js

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -1,10 +1,9 @@
 import fetch from "isomorphic-unfetch";
 
 const Cors = async (req, res) => {
-  const { url } = req.query;
   try {
-    const resProxy = await fetch(url);
-    res.status(200).send(resProxy.json());
+    const resProxy = await fetch(req.query);
+    res.status(200).json(resProxy);
   } catch (error) {
     res.status(400).send(error.toString());
   }
