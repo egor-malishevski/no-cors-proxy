d91ed6522c9481aa07a3969da17e801db064d3d9
Egor Malishevski <44815359+egor-malishevski@users.noreply.github.com>
2023-10-08 01:50:35 +0300

    code refactor

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -2,8 +2,10 @@ import fetch from "isomorphic-unfetch";
 
 const Cors = async (req, res) => {
   try {
-    const resProxy = await fetch(req.query);
-    res.status(200).json(resProxy.json());
+    const resProxy = await fetch({ ...req.query, "mode": "no-cors" );
+    const data = await resProxy.json();
+
+    res.status(200).json(data);
   } catch (error) {
     res.status(400).json({ error: error.toString() });
   }
