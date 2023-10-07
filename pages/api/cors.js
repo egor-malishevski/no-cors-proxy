4309ad76a5c41fca2dbe1364500c80337f1c8860
Egor Malishevski <44815359+egor-malishevski@users.noreply.github.com>
2023-10-08 02:04:36 +0300

    Update cors.js

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -5,9 +5,8 @@ const Cors = async (req, res) => {
   
   try {
     const resProxy = await fetch({ url, headers, method, body, "mode": "no-cors" });
-    const data = await resProxy.json();
-
-    res.status(200).send(data);
+    
+    res.status(200).send(resProxy);
   } catch (error) {
     res.status(400).send(error.toString());
   }
