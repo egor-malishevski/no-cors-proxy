258d5f9cea6ee6b1612a0f8a03a271fd07707dea
Egor Malishevski <44815359+egor-malishevski@users.noreply.github.com>
2023-10-08 01:45:13 +0300

    Update cors.js

--- pages/api/cors.js
+++ pages/api/cors.js
@@ -1,6 +1,8 @@
+import fetch from "isomorphic-unfetch";
+
 const Cors = async (req, res) => {
   try {
-    const resProxy = await fetch({ ...req.query, mode: "no-cors" });
+    const resProxy = await fetch(req.query);
     res.status(200).json(resProxy.json());
   } catch (error) {
     res.status(400).json({ error: error.toString() });
