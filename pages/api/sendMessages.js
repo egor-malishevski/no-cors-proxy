0581abb20444ed71287abfccaba699780c9d82bd
u_m1c9n <EMalishevskiy@alfabank.ru>
2023-10-08 04:03:03 +0300

    changes sendMessages endpoint

--- pages/api/sendMessages.js
+++ pages/api/sendMessages.js
@@ -3,12 +3,12 @@ import axios from "axios";
 const headers = {
   "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36",
   "Accept": "*/*",
-  //"Accept-language"    : "en,fr-FR;q=0.9,fr;q=0.8,es-ES;q=0.7,es;q=0.6,en-US;q=0.5,am;q=0.4,de;q=0.3",
+  "Accept-language"    : "en,fr-FR;q=0.9,fr;q=0.8,es-ES;q=0.7,es;q=0.6,en-US;q=0.5,am;q=0.4,de;q=0.3",
   "Origin": "https://chat.aivvm.com",
   "Referer": "https://chat.aivvm.com/",
-  //"Sec-Fetch-Dest"     : "empty",
-  //"Sec-Fetch-Mode"     : "cors",
-  //"Sec-Fetch-Site"     : "same-origin",
+  "Sec-Fetch-Dest"     : "empty",
+  "Sec-Fetch-Mode"     : "no-cors",
+  "Sec-Fetch-Site"     : "same-origin",
   "content-type": "text/plain",
 }
 
@@ -16,7 +16,6 @@ const Cors = async (req, res) => {
   try {
     const { data } = await axios('https://chat.aivvm.com/api/chat', {
       method: "POST",
-      withCredentials: false,
       headers,
       data: req ? req.body : '',
     });
